90a906dba1be0e3996cadee9a37009046b1086ce
Victor Zverovich <victor.zverovich@gmail.com>
2016-08-14 17:25:56 -0700

    Add defense

--- pokemongo.py
+++ pokemongo.py
@@ -19,158 +19,158 @@ def read_csv(data, **kwargs):
 
 # Pokémon types
 types = read_csv("""
-               Type1     Type2 Attack
-Mewtwo       Psychic         -    284
-Dragonite     Dragon    Flying    250
-Moltres         Fire    Flying    242
-Flareon         Fire         -    238
-Zapdos      Electric    Flying    232
-Exeggutor      Grass   Psychic    232
-Arcanine        Fire         -    230
-Victreebel     Grass    Poison    222
-Mew          Psychic         -    220
-Magmar          Fire         -    214
-Charizard       Fire    Flying    212
-Nidoking      Poison    Ground    204
-Gengar         Ghost    Poison    204
-Vileplume      Grass    Poison    202
-Rapidash        Fire         -    200
-Raichu      Electric         -    200
-Articuno         Ice    Flying    198
-Venusaur       Grass    Poison    198
-Machamp     Fighting         -    198
-Electabuzz  Electric         -    198
-Cloyster       Water       Ice    196
-Starmie        Water   Psychic    194
-Golduck        Water         -    194
-Gyarados       Water    Flying    192
-Jolteon     Electric         -    192
-Weezing       Poison         -    190
-Kabutops        Rock     Water    190
-Weepinbell     Grass    Poison    190
-Blastoise      Water         -    186
-Lapras         Water       Ice    186
-Magneton    Electric     Steel    186
-Vaporeon       Water         -    186
-Alakazam     Psychic         -    186
-Slowbro        Water   Psychic    184
-Nidoqueen     Poison    Ground    184
-Pinsir           Bug         -    184
-Aerodactyl      Rock    Flying    182
-Dodrio        Normal    Flying    182
-Poliwrath      Water  Fighting    180
-Omastar         Rock     Water    180
-Muk           Poison         -    180
-Snorlax       Normal         -    180
-Clefable       Fairy         -    178
-Kingler        Water         -    178
-Primeape    Fighting         -    178
-Golem           Rock    Ground    176
-Ninetales       Fire         -    176
-Scyther          Bug    Flying    176
-Seadra         Water         -    176
-Seaking        Water         -    172
-Venomoth         Bug    Poison    172
-Jynx             Ice   Psychic    172
-Haunter        Ghost    Poison    172
-Tentacruel     Water    Poison    170
-Pidgeot       Normal    Flying    170
-Dragonair     Dragon         -    170
-Fearow        Normal    Flying    168
-Ponyta          Fire         -    168
-Wigglytuff    Normal     Fairy    168
-Arbok         Poison         -    166
-Rhydon        Ground      Rock    166
-Golbat        Poison    Flying    164
-Tangela        Grass         -    164
-Hypno        Psychic         -    162
-Parasect         Bug     Grass    162
-Gloom          Grass    Poison    162
-Charmeleon      Fire         -    160
-Bellsprout     Grass    Poison    158
-Dewgong        Water       Ice    156
-Porygon       Normal         -    156
-Ivysaur        Grass    Poison    156
-Persian       Normal         -    156
-Growlithe       Fire         -    156
-Mr.Mime      Psychic     Fairy    154
-Machoke     Fighting         -    154
-Electrode   Electric         -    150
-Sandslash     Ground         -    150
-Kadabra      Psychic         -    150
-Tauros        Normal         -    148
-Hitmonlee   Fighting         -    148
-Kabuto          Rock     Water    148
-Dugtrio       Ground         -    148
-Raticate      Normal         -    146
-Wartortle      Water         -    144
-Butterfree       Bug    Flying    144
-Beedrill         Bug    Poison    144
-Kangaskhan    Normal         -    142
-Graveler        Rock    Ground    142
-Nidorino      Poison         -    142
-Marowak       Ground         -    140
-Hitmonchan  Fighting         -    138
-Farfetch'd    Normal    Flying    138
-Koffing       Poison         -    136
-Gastly         Ghost    Poison    136
-Oddish         Grass    Poison    134
-Omanyte         Rock     Water    132
-Nidorina      Poison         -    132
-Poliwhirl      Water         -    132
-Psyduck        Water         -    132
-Staryu         Water         -    130
-Magnemite   Electric     Steel    128
-Dratini       Dragon         -    128
-Charmander      Fire         -    128
-Lickitung     Normal         -    126
-Bulbasaur      Grass    Poison    126
-Pidgeotto     Normal    Flying    126
-Doduo         Normal    Flying    126
-Grimer        Poison         -    124
-Pikachu     Electric         -    124
-Paras            Bug     Grass    122
-Horsea         Water         -    122
-Mankey      Fighting         -    122
-Shellder       Water         -    120
-Machop      Fighting         -    118
-Clefairy       Fairy         -    116
-Krabby         Water         -    116
-Eevee         Normal         -    114
-Squirtle       Water         -    112
-Goldeen        Water         -    112
-Ekans         Poison         -    112
-Exeggcute      Grass   Psychic    110
-Rhyhorn       Ground      Rock    110
-Slowpoke       Water   Psychic    110
-Ditto         Normal         -    110
-Nidoran♂      Poison         -    110
-Abra         Psychic         -    110
-Venonat          Bug    Poison    108
-Poliwag        Water         -    108
-Diglett       Ground         -    108
-Tentacool      Water    Poison    106
-Geodude         Rock    Ground    106
-Vulpix          Fire         -    106
-Drowzee      Psychic         -    104
-Seel           Water         -    104
-Meowth        Normal         -    104
-Cubone        Ground         -    102
-Voltorb     Electric         -    102
-Spearow       Normal    Flying    102
-Nidoran♀      Poison         -    100
-Jigglypuff    Normal     Fairy     98
-Pidgey        Normal    Flying     94
-Rattata       Normal         -     92
-Onix            Rock    Ground     90
-Sandshrew     Ground         -     90
-Zubat         Poison    Flying     88
-Weedle           Bug    Poison     68
-Kakuna           Bug    Poison     62
-Caterpie         Bug         -     62
-Metapod          Bug         -     56
-Magikarp       Water         -     42
-Chansey       Normal         -     40
+               Type1     Type2 Attack Defense
+Bulbasaur      Grass    Poison    126     126
+Ivysaur        Grass    Poison    156     158
+Venusaur       Grass    Poison    198     200
+Charmander      Fire         -    128     108
+Charmeleon      Fire         -    160     140
+Charizard       Fire    Flying    212     182
+Squirtle       Water         -    112     142
+Wartortle      Water         -    144     176
+Blastoise      Water         -    186     222
+Caterpie         Bug         -     62      66
+Metapod          Bug         -     56      86
+Butterfree       Bug    Flying    144     144
+Weedle           Bug    Poison     68      64
+Kakuna           Bug    Poison     62      82
+Beedrill         Bug    Poison    144     130
+Pidgey        Normal    Flying     94      90
+Pidgeotto     Normal    Flying    126     122
+Pidgeot       Normal    Flying    170     166
+Rattata       Normal         -     92      86
+Raticate      Normal         -    146     150
+Spearow       Normal    Flying    102      78
+Fearow        Normal    Flying    168     146
+Ekans         Poison         -    112     112
+Arbok         Poison         -    166     166
+Pikachu     Electric         -    124     108
+Raichu      Electric         -    200     154
+Sandshrew     Ground         -     90     114
+Sandslash     Ground         -    150     172
+Nidoran♀      Poison         -    100     104
+Nidorina      Poison         -    132     136
+Nidoqueen     Poison    Ground    184     190
+Nidoran♂      Poison         -    110      94
+Nidorino      Poison         -    142     128
+Nidoking      Poison    Ground    204     170
+Clefairy       Fairy         -    116     124
+Clefable       Fairy         -    178     178
+Vulpix          Fire         -    106     118
+Ninetales       Fire         -    176     194
+Jigglypuff    Normal     Fairy     98      54
+Wigglytuff    Normal     Fairy    168     108
+Zubat         Poison    Flying     88      90
+Golbat        Poison    Flying    164     164
+Oddish         Grass    Poison    134     130
+Gloom          Grass    Poison    162     158
+Vileplume      Grass    Poison    202     190
+Paras            Bug     Grass    122     120
+Parasect         Bug     Grass    162     170
+Venonat          Bug    Poison    108     118
+Venomoth         Bug    Poison    172     154
+Diglett       Ground         -    108      86
+Dugtrio       Ground         -    148     140
+Meowth        Normal         -    104      94
+Persian       Normal         -    156     146
+Psyduck        Water         -    132     112
+Golduck        Water         -    194     176
+Mankey      Fighting         -    122      96
+Primeape    Fighting         -    178     150
+Growlithe       Fire         -    156     110
+Arcanine        Fire         -    230     180
+Poliwag        Water         -    108      98
+Poliwhirl      Water         -    132     132
+Poliwrath      Water  Fighting    180     202
+Abra         Psychic         -    110      76
+Kadabra      Psychic         -    150     112
+Alakazam     Psychic         -    186     152
+Machop      Fighting         -    118      96
+Machoke     Fighting         -    154     144
+Machamp     Fighting         -    198     180
+Bellsprout     Grass    Poison    158      78
+Weepinbell     Grass    Poison    190     110
+Victreebel     Grass    Poison    222     152
+Tentacool      Water    Poison    106     136
+Tentacruel     Water    Poison    170     196
+Geodude         Rock    Ground    106     118
+Graveler        Rock    Ground    142     156
+Golem           Rock    Ground    176     198
+Ponyta          Fire         -    168     138
+Rapidash        Fire         -    200     170
+Slowpoke       Water   Psychic    110     110
+Slowbro        Water   Psychic    184     198
+Magnemite   Electric     Steel    128     138
+Magneton    Electric     Steel    186     180
+Farfetch'd    Normal    Flying    138     132
+Doduo         Normal    Flying    126      96
+Dodrio        Normal    Flying    182     150
+Seel           Water         -    104     138
+Dewgong        Water       Ice    156     192
+Grimer        Poison         -    124     110
+Muk           Poison         -    180     188
+Shellder       Water         -    120     112
+Cloyster       Water       Ice    196     196
+Gastly         Ghost    Poison    136      82
+Haunter        Ghost    Poison    172     118
+Gengar         Ghost    Poison    204     156
+Onix            Rock    Ground     90     186
+Drowzee      Psychic         -    104     140
+Hypno        Psychic         -    162     196
+Krabby         Water         -    116     110
+Kingler        Water         -    178     168
+Voltorb     Electric         -    102     124
+Electrode   Electric         -    150     174
+Exeggcute      Grass   Psychic    110     132
+Exeggutor      Grass   Psychic    232     164
+Cubone        Ground         -    102     150
+Marowak       Ground         -    140     202
+Hitmonlee   Fighting         -    148     172
+Hitmonchan  Fighting         -    138     204
+Lickitung     Normal         -    126     160
+Koffing       Poison         -    136     142
+Weezing       Poison         -    190     198
+Rhyhorn       Ground      Rock    110     116
+Rhydon        Ground      Rock    166     160
+Chansey       Normal         -     40      60
+Tangela        Grass         -    164     152
+Kangaskhan    Normal         -    142     178
+Horsea         Water         -    122     100
+Seadra         Water         -    176     150
+Goldeen        Water         -    112     126
+Seaking        Water         -    172     160
+Staryu         Water         -    130     128
+Starmie        Water   Psychic    194     192
+Mr.Mime      Psychic     Fairy    154     196
+Scyther          Bug    Flying    176     180
+Jynx             Ice   Psychic    172     134
+Electabuzz  Electric         -    198     160
+Magmar          Fire         -    214     158
+Pinsir           Bug         -    184     186
+Tauros        Normal         -    148     184
+Magikarp       Water         -     42      84
+Gyarados       Water    Flying    192     196
+Lapras         Water       Ice    186     190
+Ditto         Normal         -    110     110
+Eevee         Normal         -    114     128
+Vaporeon       Water         -    186     168
+Jolteon     Electric         -    192     174
+Flareon         Fire         -    238     178
+Porygon       Normal         -    156     158
+Omanyte         Rock     Water    132     160
+Omastar         Rock     Water    180     202
+Kabuto          Rock     Water    148     142
+Kabutops        Rock     Water    190     190
+Aerodactyl      Rock    Flying    182     162
+Snorlax       Normal         -    180     180
+Articuno         Ice    Flying    198     242
+Zapdos      Electric    Flying    232     194
+Moltres         Fire    Flying    242     194
+Dratini       Dragon         -    128     110
+Dragonair     Dragon         -    170     152
+Dragonite     Dragon    Flying    250     212
+Mewtwo       Psychic         -    284     202
+Mew          Psychic         -    220     220
 """, index_col=0)
 
 # Move (attack) data from http://www.pokemongodb.net/.
@@ -422,12 +422,17 @@ if __name__ == '__main__':
     soup = fetch_bs_html(
         'http://www.pokemongodb.net/2016/07/pokemon-by-attack.html')
     pokemon_list = []
-    for _, _, pokemon, type1, type2, _, attack, _ in get_rows(soup):
+    for no, _, pokemon, type1, type2, _, attack, defense in get_rows(soup):
         pokemon = pokemon.contents[0].replace(' ', '')
-        pokemon_list.append({'Pokemon': pokemon, 'Type1': link2type(type1),
-                             'Type2': link2type(type2), 'Attack': attack})
-    df = pd.DataFrame(pokemon_list,
-                      columns=('Pokemon', 'Type1', 'Type2', 'Attack'))
+        p = {
+          'No': no, 'Pokemon': pokemon,
+          'Type1': link2type(type1), 'Type2': link2type(type2),
+          'Attack': attack, 'Defense': defense
+        }
+        pokemon_list.append(p)
+    columns = ('Pokemon', 'Type1', 'Type2', 'Attack', 'Defense')
+    pokemon_list.sort(key=lambda p: p['No'])
+    df = pd.DataFrame(pokemon_list, columns=columns)
     content = update_data(content, df.set_index('Pokemon'), 'types')
     # Write data.
     with open(__file__, 'w') as f:
